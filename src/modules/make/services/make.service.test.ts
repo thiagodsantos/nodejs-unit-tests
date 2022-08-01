6955f1c6802243e3b0d6f6ff932540b2bd293243
Thiago Santos <thiago.dsantos@outlook.com>
2022-08-01 02:22:49 -0300

    feat: Added expcetion cases unit tests

--- src/modules/make/services/make.service.test.ts
+++ src/modules/make/services/make.service.test.ts
@@ -1,20 +1,21 @@
+import sinon, { createSandbox } from 'sinon';
 import { expect } from "expect";
 import { afterEach } from "mocha";
-import sinon, { mock, SinonMockStatic } from 'sinon';
 import { MakeService } from "@modules/make/services/make.services";
 import { MakeRepository } from "@modules/make/infrastructure/typeorm/make.repository";
 import { MakeEntity } from "@modules/make/entities/make.entity";
+import { MakeException } from "@modules/make/exceptions/make.exception";
 
 describe('MakeService', () => {
-  let mockMakeRepository: SinonMockStatic[MakeRepository];
+  const sandbox = createSandbox();
+  const makeRepository = new MakeRepository();
   
   beforeEach(() => {
-    const makeRepository = new MakeRepository();
-    mockMakeRepository = sinon.mock(makeRepository);
+    sandbox.verify();
   });
   
   afterEach(() => {
-    mockMakeRepository = sinon.restore();
+    sandbox.restore();
   })
   
   it('getAll should works', async () => {
@@ -24,8 +25,7 @@ describe('MakeService', () => {
     
     const expected = [makeEntity];
   
-    const makeRepository = new MakeRepository();
-    sinon.mock(makeRepository)
+    sandbox.mock(makeRepository)
       .expects("getAll")
       .returns(expected);
   
@@ -33,5 +33,36 @@ describe('MakeService', () => {
     const result = await service.getAll();
     
     expect(result).toBe(expected);
+  });
+  
+  it('getById should works', async () => {
+    const makeEntity = new MakeEntity();
+    makeEntity.id = "d5787550-74a5-40c6-a7e2-0d0e54f75709";
+    makeEntity.name = "Audi";
+    
+    const expected = makeEntity;
+    
+    sandbox.mock(makeRepository)
+      .expects("getById")
+      .returns(expected);
+    
+    const service = new MakeService(makeRepository);
+    const result = await service.getById(makeEntity.id);
+    
+    expect(result).toBe(expected);
+  });
+  
+  it('getById should throw exception', async () => {
+    const expected = null;
+    
+    sandbox.mock(makeRepository)
+      .expects("getById")
+      .returns(expected);
+    
+    const service = new MakeService(makeRepository);
+  
+    await expect(async () => {
+      return await service.getById("d5787550-74a5-40c6-a7e2-0d0e54f75709")
+    }).rejects.toThrow();
   });
 });
