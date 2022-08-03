7ab8eaaf131445f99a7f45a4fc58c7fb9bcbe6c9
Thiago Santos <thiago.dsantos@outlook.com>
2022-08-02 23:47:47 -0300

    fix: eslint unused rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -6,7 +6,7 @@ module.exports = {
   plugins: ['@typescript-eslint/eslint-plugin'],
   extends: [
     'plugin:@typescript-eslint/eslint-recommended',
-    'plugin:@typescript-eslint/recommended',
+    'plugin:@typescript-eslint/recommended'
   ],
   overrides: [
     {
@@ -23,23 +23,6 @@ module.exports = {
         '@typescript-eslint/explicit-module-boundary-types': 'off',
         '@typescript-eslint/no-unused-vars': 'off',
         '@typescript-eslint/ban-types': 'off',
-      },
-    },
-    {
-      files: ['**/*.spec.ts', 'integration/**/*.ts'],
-      parser: '@typescript-eslint/parser',
-      parserOptions: {
-        project: 'tsconfig.spec.json',
-        sourceType: 'module',
-      },
-      rules: {
-        '@typescript-eslint/interface-name-prefix': 'off',
-        '@typescript-eslint/explicit-function-return-type': 'off',
-        '@typescript-eslint/no-explicit-any': 'off',
-        '@typescript-eslint/explicit-module-boundary-types': 'off',
-        '@typescript-eslint/no-unused-vars': 'off',
-        '@typescript-eslint/ban-types': 'off',
-        '@typescript-eslint/no-empty-function': 'off',
       },
     }
   ]
